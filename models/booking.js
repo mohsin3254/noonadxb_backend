2c4f81b8041e5ad1483e632412895dfa71e32fc8
mohsin3254 <91689334+mohsin3254@users.noreply.github.com>
2024-08-14 06:06:12 +0500

    updatedBookingroutes

--- models/booking.js
+++ models/booking.js
@@ -27,35 +27,6 @@ const Booking = mongoose.model("Booking", bookingSchema);
 
 module.exports = Booking;
 */
-/*
-const mongoose = require("mongoose");
-
-const bookingSchema = new mongoose.Schema(
-  {
-    service: String,
-    serviceid: mongoose.Schema.Types.ObjectId,
-    name: String,
-    phone: String,
-    date: String,
-    time: String,
-    address: String,
-    totalamount: Number,
-    transactionid: String,
-    userid: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      default: null,
-      required: false, // Make userid optional
-    },
-    status: { type: String, default: "booked" },
-  },
-  { timestamps: true }
-);
-
-const Booking = mongoose.model("Booking", bookingSchema);
-
-module.exports = Booking;
-*/
 
 const mongoose = require("mongoose");
 
